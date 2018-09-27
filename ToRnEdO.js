c9cfdacdd65e90cd543ac2f6e1d57f77dfede72e
ml5om <43390029+ml5om@users.noreply.github.com>
2018-09-27 16:51:22 +0300

    Update ToRnEdO.js

--- ToRnEdO.js
+++ ToRnEdO.js
@@ -64,9 +64,8 @@ client.on('message', async msg => {
                     const embed1 = new Discord.RichEmbed()
                     .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
-  .setFooter(`${msg.guild.name}`)                       
  
-                    .setFooter("-")
+                    .setFooter("by => الـجنـرال#2666")         .setFooter(`${msg.guild.name}`)
                     msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
                    
                     // eslint-disable-next-line max-depth
